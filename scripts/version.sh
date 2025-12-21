ca3cdb29f1ba4a7829cb0c3452d3d859bafad0f8
Mark <remarkablemark@users.noreply.github.com>
2025-12-20 19:46:57 -0500

    chore(scripts): add version update script for Maestro

---
+++ scripts/version.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+CURRENT_VERSION=$(yq .inputs.version.default action.yml)
+
+echo "Current version: $CURRENT_VERSION"
+
+LATEST_VERSION=$(
+  gh release list \
+    --repo mobile-dev-inc/Maestro \
+    --limit 1 \
+    --exclude-pre-releases \
+    --json name \
+    --jq '.[0].name' | \
+  cut -c5-
+)
+
+echo "Latest version: $LATEST_VERSION"
+
+if [[ $CURRENT_VERSION == $LATEST_VERSION ]]; then
+  echo "Maestro version has not changed. Exiting"
+  exit
+fi
+
+git stash
+
+FILES=$(git grep -l "$CURRENT_VERSION" -- ':!.github' ':!CHANGELOG.md' ':!package*.json')
+
+if [[ $(uname) == 'Darwin' ]]; then
+  echo "$FILES" | xargs sed -i '' -e "s/$CURRENT_VERSION/$LATEST_VERSION/g"
+else
+  echo "$FILES" | xargs sed -i -e "s/$CURRENT_VERSION/$LATEST_VERSION/g"
+fi
+
+npm run build
+
+echo 'Creating PR...'
+BRANCH="build/version-$LATEST_VERSION"
+git checkout -b $BRANCH
+git commit -am \
+  "build(deps): bump maestro from $CURRENT_VERSION to $LATEST_VERSION" \
+  -m "Release-As: $(jq -r .version package.json | awk -F. '/[0-9]+\./{$NF++;print}' OFS=.)" \
+  -m "https://github.com/mobile-dev-inc/Maestro/releases/tag/v$LATEST_VERSION"
+git push --force origin $BRANCH
+gh pr create --assignee remarkablemark --fill --reviewer remarkablemark
+
+git stash pop || true
